6db12c2f4808f2f4ca069637e2c2e7489f6db54e
Freedteck <freedteck@gmail.com>
2023-10-07 14:59:19 +0100

    add icons, edit html and css, modify javascript

--- js/script.js
+++ js/script.js
@@ -5,17 +5,13 @@ function Book(title, author, pages, haveRead) {
     this.author = author;
     this.pages = pages;
     this.haveRead = haveRead;
-
-    this.info = function () {
-        return `${title} by ${author}, ${pages} pages, ${haveRead} yet`
-    }
 }
 
-const addNew = document.querySelector('#new');
-const myDialog = document.getElementById('my-dialog')
-const addBook = document.querySelector('form button');
+const dialog = document.getElementById('my-dialog')
+const newBookButton = document.getElementById('new')
+const addBook = document.getElementById('add')
+const closeModal = document.getElementById('close')
 
-const input = document.querySelectorAll('input')
 const title = document.getElementById('title');
 const author = document.getElementById('author');
 const pages = document.getElementById('pages');
@@ -23,62 +19,100 @@ const check = document.getElementById('read')
 
 const container = document.querySelector('.container')
 
-function addBookToLibrary() {
-    const book = new Book(title.value, author.value, pages.value, )
-    myLibrary.push(book)
+newBookButton.addEventListener('click', showForm)
 
+addBook.addEventListener('click', () => {
 
-    showBooks();
+    if (title.validity.valid && author.validity.valid && pages.validity.valid) {
+        addBookToLibrary()
+    } else {
+        alert('Enter All Fields')
+    }
+})
+
+closeModal.addEventListener('click', () => {
+    dialog.close()
+})
+
+function showForm() {
+    title.value = ''
+    author.value = ''
+    pages.value = ''
+    check.checked = false
+    dialog.showModal()
 }
 
 function showBooks() {
     container.innerHTML = ''
 
-    myLibrary.forEach((library, index) => {
+    myLibrary.forEach((bookLibrary, index) => {
         const card = document.createElement('div')
+        card.classList.add('card')
+
         const titleElem = document.createElement('h3')
         const authorElem = document.createElement('p')
         const pagesElem = document.createElement('p')
-        const haveReadElem = document.createElement('button')
-        const deleteBtn = document.createElement('button')
+        const innerDiv = document.createElement('div')
+        const deleteIcon = document.createElement('img')
+        const readIcon = document.createElement('img')
 
+        titleElem.textContent = `Title: ${bookLibrary.title}`
+        authorElem.textContent = `By: ${bookLibrary.author}`
+        pagesElem.textContent = `${bookLibrary.pages} Pages`
 
-        titleElem.textContent = `Title: ${library.title}`;
-        authorElem.textContent = `By: ${library.author}`
-        pagesElem.textContent = `Pages: ${library.pages} pages`
-        deleteBtn.textContent = `Delete Book`
+        deleteIcon.setAttribute('src', "icons/delete.svg")
+        deleteIcon.setAttribute('src', "icons/delete.svg")
+        deleteIcon.setAttribute('width', "30")
 
-        if (check.checked) {
-            haveReadElem.style.backgroundColor = "green"
-            haveReadElem.textContent = `I have Read it: ${library.haveRead}`
-        }
+        readIcon.setAttribute('src', "icons/read.svg")
+        readIcon.setAttribute('alt', "Book Read")
+        readIcon.setAttribute('width', "30")
+
         card.appendChild(titleElem)
         card.appendChild(authorElem)
         card.appendChild(pagesElem)
-        card.appendChild(haveReadElem)
-        card.appendChild(deleteBtn)
+        card.appendChild(innerDiv)
+        innerDiv.appendChild(deleteIcon)
+        innerDiv.appendChild(readIcon)
         container.appendChild(card)
 
-        deleteBtn.addEventListener('click', () => {
+        if (bookLibrary.haveRead) {
+            readIcon.setAttribute('src', "icons/read-green.svg")
+        }
+        deleteIcon.addEventListener('click', () => {
             deleteBook(index)
         })
+
+        readIcon.addEventListener('click', () => {
+            if (bookLibrary.haveRead) {
+                readIcon.setAttribute('src', "icons/read.svg")
+                bookLibrary.haveRead = !bookLibrary.haveRead
+            } else {
+                readIcon.setAttribute('src', "icons/read-green.svg")
+                bookLibrary.haveRead = !bookLibrary.haveRead
+            }
+        })
     })
+
+}
+
+function addBookToLibrary() {
+    const titleValue = title.value.trim()
+    const authorValue = author.value.trim()
+    const pagesValue = pages.value.trim()
+    let checkBox = check.checked
+    const book = new Book(titleValue, authorValue, pagesValue, checkBox)
+    myLibrary.push(book)
+
+    showBooks()
 }
 
 function deleteBook(index) {
     myLibrary.splice(index, 1)
     showBooks()
 }
-addNew.addEventListener('click', () => {
-    title.value = ''
-    author.value = ''
-    pages.value = ''
-    myDialog.showModal()
-})
 
-addBook.addEventListener('click', (e) => {
-    if (title.validity.valid && author.validity.valid && pages.validity.valid) {
-        addBookToLibrary()
-    }
-})
-
+function toogleRead(library) {
+    library.haveRead = "no"
+    console.log(library.haveRead);
+}
